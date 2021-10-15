88687f55427e13c8b39ab72763c92fa3ffebe454
jo-pol <jo-pol@users.noreply.github.com>
2021-10-15 09:46:57 +0200

    official values
    from https://github.com/DANS-KNAW/dataverse/blob/eb59a1100a7879d2dcb364441e7b313a863e149c/src/main/java/edu/harvard/iq/dataverse/Dataverse.java#L74

--- src/main/java/nl/knaw/dans/testjackson/DataverseType.java
+++ src/main/java/nl/knaw/dans/testjackson/DataverseType.java
@@ -3,9 +3,18 @@ package nl.knaw.dans.testjackson;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum DataverseType {
+  //@formatter:off
+  RESEARCHERS("RESEARCHERS"),
+  RESEARCH_PROJECTS("RESEARCH PROJECTS"),
+  JOURNALS("JOURNALS"),
+  ORGANIZATIONS_INSTITUTIONS("ORGANIZATIONS INSTITUTIONS"),
+  TEACHING_COURSES("TEACHING COURSES"),
   UNCATEGORIZED("UNCATEGORIZED"),
-  ARCHEOLOGY("ARCH EOLOGY"),
-  HUMANITIES("HUMANITIES");
+  LABORATORY("LABORATORY"),
+  RESEARCH_GROUP("RESEARCH GROUP"),
+  DEPARTMENT("DEPARTMENT");
+  //@formatter:on
+
   private String value;
 
   DataverseType(String displayValue) {
@@ -13,8 +22,13 @@ public enum DataverseType {
   }
 
   @JsonValue
-  public String value() { return value; }
+  public String value() {
+    return value;
+  }
 
-  @Override public String toString() { return value; }
+  @Override
+  public String toString() {
+    return value;
+  }
 }
 
